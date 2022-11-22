7a0b8c1ba4df47c199070c704086472ff724fab0
Olga Ukhanova <uhole4ka@rambler.ru>
2022-11-22 23:21:24 +0300

    Добавили решение задач 25,27,29

---
+++ Z27/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 27: Напишите программу, которая принимает на 
+//вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+int x=Convert.ToInt32(Console.ReadLine());
+string a = Convert.ToString(x);
+int sum=0;
+
+for (int i = 0; i < a.Length; i++)
+{
+sum+=int.Parse(Convert.ToString(a[i]));
+}
+Console.WriteLine(sum +" "); 
+
+
+
+
+
---
+++ Z29/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из N 
+//элементов и выводит их на экран. Вывод сделать отдельным методом.
+
+Console.Write("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[n];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i]= new Random().Next(0,100);
+}
+PrintArray(numbers);
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i=0;i<array.Length;i++)
+    {Console.Write($"{numbers[i]} ");}
+    Console.WriteLine("]");
+}
+
